d80451cb644496e7df2a368f46200453c7ee86c3
pko104 <peterrko1004@gmail.com>
2016-11-07 21:22:58 -0500

    profile view css

--- app/assets/javascripts/experience.js
+++ app/assets/javascripts/experience.js
@@ -1,32 +1,32 @@
-// $(function() {
-//   var $form = $('#new-experience');
-//   var $formSubmit = $('#submit-experience')
+$(function() {
+  var $form = $('#new-experience');
+  var $formSubmit = $('#submit-experience')
 
-//   $formSubmit.on('click', function(e) {
-//     e.preventDefault();
+  $formSubmit.on('click', function(e) {
+    e.preventDefault();
 
-//     var userId = $form.attr('data-user-id');
-//     var titleMessage = $('#title').val();
-//     var companynameMessage = $('#company_name').val();
-//     var dateMessage = $('#date').val();
-//     var descriptionMessage = $('#description').val();
+    var userId = $form.attr('data-user-id');
+    var titleMessage = $('#title').val();
+    var companynameMessage = $('#company_name').val();
+    var dateMessage = $('#date').val();
+    var descriptionMessage = $('#description').val();
 
-//     $.ajax({
-//       url: '/profile_pages/'+ userId +'/profile.json',
-//       data: {title: titleMessage,
-//         company_name: companynameMessage,
-//         description: descriptionMessage,
-//         date: dateMessage
-//       },
-//       dataType: 'json',
-//       method: 'POST'
-//     }).
-//     success(function(data) {
-//       var messageContainer = $("#current-experience");
-//       messageContainer.prepend(data.title + data.company_name + data.date + data.description);
-//     });
-//   });
+    $.ajax({
+      url: '/profile_pages/'+ userId +'/profile.json',
+      data: {title: titleMessage,
+        company_name: companynameMessage,
+        description: descriptionMessage,
+        date: dateMessage
+      },
+      dataType: 'json',
+      method: 'POST'
+    }).
+    success(function(data) {
+      var messageContainer = $("#current-experience");
+      messageContainer.prepend(data.title + data.company_name + data.date + data.description);
+    });
+  });
 
-// });
+});
 
 
--- app/assets/javascripts/experience_click_box.js
+++ app/assets/javascripts/experience_click_box.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-  $("#new_experience").hide();
-  $("#experience-opener").hide();
-    $("#experience-opener").click(function() {
-        $("#experience-opener").hide()
-        $("#new_experience").show();
-        return false
-    });
+  // $("#new_experience").hide();
+  // $("#experience-opener").hide();
+  //   $("#experience-opener").click(function() {
+  //       $("#experience-opener").hide()
+  //       $("#new_experience").show();
+  //       return false
+  //   });
 
-    $("#experience-closer").click(function() {
-        $("#new_experience").hide();
-        $("#experience-opener").show()
-        return false
-    });
+  //   $("#experience-closer").click(function() {
+  //       $("#new_experience").hide();
+  //       $("#experience-opener").show()
+  //       return false
+  //   });
 });
